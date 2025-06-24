b3178f6c6e9fc75ed92f98ddf6101883fa3fb2ca
your name <your email>
2025-06-24 11:32:59 +0530

    project

--- client/src/Redux/action.js
+++ client/src/Redux/action.js
@@ -117,7 +117,7 @@ const getAllUserDataFailure = (payload) => {
 export const getAllUserDataFromBackend = (payload) => (dispatch) => {
   dispatch(getAllUserDataRequest());
   axios
-    .get("https://mern-quiz-server-sudhir.onrender.com/getuser")
+    .get("https://deep-quiz-3.onrender.com/getuser")
     .then((res) => {
       dispatch(getAllUserDataSuccess(res.data));
     })
@@ -128,7 +128,7 @@ export const getAllUserDataFromBackend = (payload) => (dispatch) => {
 
 export const deleteUserByAdmin = (payload) => (dispatch) => {
   axios
-    .delete(`https://mern-quiz-server-sudhir.onrender.com/${payload}`)
+    .delete(`https://deep-quiz-3.onrender.com/${payload}`)
     .then((response) => {
       dispatch(getAllUserDataFromBackend());
     })
@@ -155,14 +155,14 @@ export const postQuizObj = (obj) => (dispatch) => {
 export const fetchQuizDataFrombackend = () => (dispatch) => {
   dispatch(fetchQuizRequest());
   axios
-    .get("https://mern-quiz-server-sudhir.onrender.com/api/quiz")
+    .get("https://deep-quiz-3.onrender.com/api/quiz")
     .then((res) => dispatch(fetchQuizSuccess(res.data)))
     .catch((err) => dispatch(fetchQuizFailure(err)));
 };
 
 export const getQuiz = (params) => (dispatch) => {
   axios
-    .get(`https://mern-quiz-server-sudhir.onrender.com/quiz/${params.id}`)
+    .get(`https://deep-quiz-3.onrender.com/quiz/${params.id}`)
     .then((res) => {
     
       dispatch(fetchQuizSuccess(res.data));
@@ -203,7 +203,7 @@ export const postQuizResult = (obj) => (dispatch) => {
   const { quizId, userId, quizResult } = obj;
   dispatch(postUserResultRequest());
   axios
-    .post(`https://mern-quiz-server-sudhir.onrender.com/userResult/${userId}`, obj)
+    .post(`https://deep-quiz-3.onrender.com/userResult/${userId}`, obj)
     .then((res) => {
       dispatch(postUserResultSuccess(res.data));
     })
